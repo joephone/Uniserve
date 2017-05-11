c23cd007bb58c067a1a25196ea7b438b3d698c4b
joephone <joephonechen@gmail.com>
2017-05-11 10:57:30 +0800

    init project
    init project

---
+++ app/src/main/java/universe/joephone/com/universe/MainActivity.java
@@ -0,0 +1,13 @@
+package universe.joephone.com.universe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
