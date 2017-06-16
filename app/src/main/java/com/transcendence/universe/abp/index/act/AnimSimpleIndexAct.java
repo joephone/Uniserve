88975c72c648153305638fa93b68412057d08b23
joephone <joephonechen@gmail.com>
2017-06-16 18:22:33 +0800

    AnimIndex
    AnimIndex

---
+++ app/src/main/java/com/transcendence/universe/abp/index/act/AnimSimpleIndexAct.java
@@ -0,0 +1,66 @@
+package com.transcendence.universe.abp.index.act;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.transcendence.universe.R;
+import com.transcendence.universe.abp.classic.toast.act.BToastMainAct;
+import com.transcendence.universe.abp.index.adapter.IndexAdapter;
+import com.transcendence.universe.util.Loger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by joephone on 2017/6/16.
+ */
+public class AnimSimpleIndexAct extends Activity implements IndexAdapter.IndexEvent {
+
+    @Bind(R.id.mRecyclerView)
+    RecyclerView mRecyclerView;
+
+    private String tag = this.getClass().getName();
+    private Activity mActivity;
+    private IndexAdapter adapter;
+    private List<String> sourceList = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+        ButterKnife.bind(this);
+
+        sourceList.add("BToast");
+
+        mActivity = this;
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        adapter = new IndexAdapter(this,sourceList);
+        mRecyclerView.setAdapter(adapter);
+        adapter.setEvent(this);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Loger.i(tag,"onItemClick");
+        Intent intent = new Intent();
+        switch (position){
+            case 0:
+                intent = new Intent(mActivity,BToastMainAct.class);
+                break;
+            case 1:
+                break;
+        }
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/transcendence/universe/abp/main/act/AboutAct.java
@@ -0,0 +1,18 @@
+package com.transcendence.universe.abp.main.act;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.transcendence.universe.R;
+
+public class AboutAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+
+    }
+}
