547cba552966b5dba300c1dababe7b93018054b8
joephone <270136049@qq.com>
2019-03-22 17:48:19 +0800

    startActivity
    startActivity

--- app/src/main/java/com/transcendence/universe/abp/main/act/BaseActivity.java
+++ app/src/main/java/com/transcendence/universe/abp/main/act/BaseActivity.java
@@ -205,4 +205,44 @@ public class BaseActivity extends AppCompatActivity {
     public void onPermissionsDenied(int requestCode) {
     }
 
+
+    /**
+     * 通过Class跳转界面
+     **/
+    public void startActivity(Class<?> cls) {
+        startActivity(cls, null);
+    }
+
+    /**
+     * 含有Bundle通过Class跳转界面
+     **/
+    public void startActivity(Class<?> cls, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+
+    /**
+     * 通过Class跳转界面
+     **/
+    public void startActivityForResult(Class<?> cls, int requestCode) {
+        startActivityForResult(cls, null, requestCode);
+    }
+
+    /**
+     * 含有Bundle通过Class跳转界面
+     **/
+    public void startActivityForResult(Class<?> cls, Bundle bundle, int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+
 }
